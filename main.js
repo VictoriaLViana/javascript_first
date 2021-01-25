376bb0400b8324e60d1a99b548ec0f410cc2ec44
Victoria Rebelo <76655929+VictoriaLViana@users.noreply.github.com>
2021-01-25 16:41:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function clicou(){
+    alert ("Obrigada por cliclar");
+}
+
+
+/*
+var nome = "Victoria Viana";
+var idade =  26;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo"
+//alert (nome + "tem" + idade + "anos");
+//alert (idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase);
+console.log(frase.replace ("Japão", "Brasil"));
+var lista = ["maçã","melância","melão","acerola"];
+console.log(lista);
+console.log(lista[3]);
+alert(lista[2]);
+lista.pop();
+//lista.push("uva");
+console.log(lista.length);
+*/
+//var fruta={nome: "maçã", cor: "vermelha"};
+//console.log(fruta);
+/*var idade = prompt("Qual a sua idade?")
+if (idade>=18){
+    alert("pode entrar, maior de idade");
+}
+else{
+    alert("não pode entrar, menor de idade");
+}
+*/
+/*var count = 0;
+while (count<5){
+    console.log(count);
+    count++;
+};*/
+/*var count;
+for (count=0; count <=5; count++){
+    console.log (count);
+}
+*/
